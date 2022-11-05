30d4bb7117259aea6eeb57b57c0b5ead1f2247da
Liam McLuckie <liammcluckie@hotmail.co.uk>
2022-11-05 10:53:35 +0000

    Add timeline template markup

--- app/public/wp-content/themes/wedding-site/front-page.php
+++ app/public/wp-content/themes/wedding-site/front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
 <?php include_once 'template-parts/hero.php'; ?>
+<?php include_once 'template-parts/timeline.php'; ?>
 
 <?php get_footer(); ?>
---
+++ app/public/wp-content/themes/wedding-site/template-parts/timeline.php
@@ -0,0 +1,84 @@
+<section class="container">
+    <div class="row">
+        <ul class="timeline">
+            <li class="column timeline--item">
+                <div class="timings">
+                    <div class="timings--icon">
+                        <img 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/icons/arrival.png" 
+                            alt="Icon illustration depicting arrival"
+                            title="Icon created by WEBTECHHOPS LLP from Noun Project"
+                        >
+                    </div>
+                    <h2>Arrival</h2>
+                    <time>11.30</time>
+                </div>
+            </li>
+            <li class="column timeline--item">
+                <div class="timings">
+                    <div class="timings--icon">
+                        <img 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/icons/ceremony.png" 
+                            alt="Icon illustration depicting a wedding ceremony"
+                            title="Icon created by Jino from Noun Project"
+                        >
+                    </div>
+                    <h2>Ceremony</h2>
+                    <time>13:00</time>
+                </div>
+            </li>
+            <li class="column timeline--item">
+                <div class="timings">
+                    <div class="timings--icon">
+                        <img 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/icons/games.png" 
+                            alt="Icon illustration games"
+                            title="Icon created by Amethyst Studio from Noun Project"
+                        >
+                    </div>
+                    <h2>Drinks/Games</h2>
+                    <time>14:00</time>
+                </div>
+            </li>
+            <li class="column timeline--item">
+                <div class="timings">
+                    <div class="timings--icon">
+                        <img 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/icons/food.png" 
+                            alt="Icon illustration depicting food"
+                            title="Icon created by Maxicons from Noun Project"
+                        >
+                    </div>
+                    <h2>Food</h2>
+                    <time>15:00</time>
+                </div>
+            </li>
+            <li class="column timeline--item">
+                <div class="timings">
+                    <div class="timings--icon">
+                        <img 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/icons/toast.png" 
+                            alt="Icon illustration depicting drink toasts"
+                            title="Icon created by Made by Made from Noun Project"
+                        >
+                    </div>
+                    <h2>Speeches</h2>
+                    <time>16:00</time>
+                </div>
+            </li>
+            <li class="column timeline--item">
+                <div class="timings">
+                    <div class="timings--icon">
+                        <img 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/icons/party.png" 
+                            alt="Icon illustration depicting a party"
+                            title="Icon created by Berkah Icon from Noun Project"
+                        >
+                    </div>
+                    <h2>Party</h2>
+                    <time>18:00</time>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
