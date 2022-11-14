3fbf520e7488467c92290ef23b2f2d5d4c18d0d7
Liam McLuckie <liammcluckie@hotmail.co.uk>
2022-11-14 20:33:14 +0000

    WIP add form template

---
+++ app/public/wp-content/themes/wedding-site/templates/form.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * Template Name: RSVP Form
+     */
+?>
+<?php get_header(); ?>
+
+<section class="container">
+    <div class="row">
+        <div class="column">
+            <?php echo the_content(get_the_id()); ?>
+        </div>
+    </div>
+</section>
