3e354c47930a77bc8188313facb254478d9aa62e
Liam McLuckie <liammcluckie@hotmail.co.uk>
2022-12-29 18:00:16 +0000

    WIP add page header component

---
+++ app/public/wp-content/themes/wedding-site/page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<?php include_once 'template-parts/page-header.php'; ?>
+
+<?php get_footer(); ?>
---
+++ app/public/wp-content/themes/wedding-site/template-parts/page-header.php
@@ -0,0 +1,12 @@
+<header class="container no-margin-bottom">
+    <div class="row flex-centered">
+        <div class="column two-thirds center">
+            <h1 class="page-title"><?php the_title(); ?></h1>
+            <?php if ($intro = get_field('page_intro')) : ?>
+                <p class="page-intro"><?php echo $intro; ?></p>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="header-illustration-top"></div>
+    <div class="header-illustration-bottom"></div>
+</header>
