bbadf8a01bd0d1328cd95f1af9a402b06b0c1ad2
Liam McLuckie <liammcluckie@hotmail.co.uk>
2022-12-29 12:21:31 +0000

    Add footer

--- app/public/wp-content/themes/wedding-site/footer.php
+++ app/public/wp-content/themes/wedding-site/footer.php
@@ -1,5 +1,26 @@
 <?php wp_footer();?>
 
+<?php $email = base64_encode('ansell.mcluckie@gmail.com'); ?>
+
+    <footer>
+        <div class="row">
+            <div class="column one-half center l-one-whole">
+                <p>E | <a class="encoded-mailto encoded-email">Please enable Javascript in your browser to view this.</a></p>
+            </div>
+            <div class="column one-half center l-one-whole">
+                <p>Design & Build | <a href="https://github.com/liammcluckie" target="_blank" rel="noopener">Liam McLuckie</a></p>
+            </div>
+        </div>
+    </footer>
+
 </main>
+    <script>
+        // screw you bots
+        const emailAddress = atob('<?php echo htmlspecialchars($email); ?>');
+        document.querySelectorAll('.encoded-mailto').forEach((el) => {
+            el.href = `mailto:${emailAddress}`;
+            el.innerHTML = emailAddress;
+        });
+    </script>
 </body>
 </html>
