180e9ebe1789d52176690f1ea8f4f56def5ffbe7
Liam McLuckie <liammcluckie@hotmail.co.uk>
2023-01-08 17:42:17 +0000

    Add extra grid spacing utility class

--- app/public/wp-content/themes/wedding-site/template-parts/page-header.php
+++ app/public/wp-content/themes/wedding-site/template-parts/page-header.php
@@ -1,6 +1,6 @@
 <header class="container no-margin-bottom">
     <div class="row flex-centered">
-        <div class="column two-thirds center">
+        <div class="column two-thirds center extra-column-spacing-bottom">
             <h1 class="page-title"><?php the_title(); ?></h1>
             <?php if ($intro = get_field('page_intro')) : ?>
                 <p class="page-intro"><?php echo $intro; ?></p>
