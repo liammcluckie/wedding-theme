e2523565851ce56013be42b57548e0292b381f58
Liam McLuckie <liammcluckie@hotmail.co.uk>
2023-03-18 18:27:41 +0000

    Add optional title to image component

--- app/public/wp-content/themes/wedding-site/template-parts/image.php
+++ app/public/wp-content/themes/wedding-site/template-parts/image.php
@@ -2,6 +2,10 @@
     <div class="row flex-centered">
         <div class="column two-thirds m-one-whole">
             <?php echo wedd_render_wp_image($image['ID'], 'large'); ?>
+
+            <?php if ($image_desc) : ?>
+                <p class="image-desc"><?php echo $image_desc; ?></p>
+            <?php endif ?>
         </div>
     </div>
 </section>
