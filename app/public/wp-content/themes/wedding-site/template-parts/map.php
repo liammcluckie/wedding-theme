725c91b48e2f84f01773294d0c381b1ae4161537
Liam McLuckie <liammcluckie@hotmail.co.uk>
2023-03-18 18:27:18 +0000

    Add google map template part

---
+++ app/public/wp-content/themes/wedding-site/template-parts/map.php
@@ -0,0 +1,25 @@
+<section>
+    <div class="row flex-centered">
+        <div class="column twh-thirds">
+            <h2>Getting Here</h2>
+        </div>
+    </div>
+    <div class="row flex-centered">
+        <div class="column two-thirds xl-one-whole">
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe src="https://maps.google.com/maps?q=Eco%20Park&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=&amp;output=embed" id="gmap_canvas" frameborder="0" scrolling="no" style="width: 100%; height: 400px;">
+                    </iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row flex-centered">
+        <div class="column two-thirds xl-one-whole">
+            <p class="location">
+                <strong>Location&nbsp;|</strong>
+            </p>
+            <address class="address">Mount Pleasant Eco Park, Truro, TR4 8HL, Cornwall</address>
+        </div>
+    </div>
+</section>
