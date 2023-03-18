927af9570edb7ec1eba319379a6359e0060b007f
Liam McLuckie <liammcluckie@hotmail.co.uk>
2023-03-18 13:38:18 +0000

    Wrap content in anchor tag

--- app/public/wp-content/themes/wedding-site/template-parts/text-beside-image.php
+++ app/public/wp-content/themes/wedding-site/template-parts/text-beside-image.php
@@ -10,31 +10,31 @@
                 <?php endif; ?>
 
                 <div class="tbi-content">
-                    <div class="tbi-content--inner">
-
-                        <?php if ($tbi_title) : ?>
-                            <h3 class="tbi-title"><?php echo $tbi_title; ?></h3>
-                        <?php endif; ?>
-
-                        <?php if ($tbi_intro) : ?>
-                            <p class="tbi-intro"><?php echo $tbi_intro; ?></p>
-                        <?php endif; ?>
-
-                        <?php if ($tbi_content) : ?>
-                            <p class="tbi-body"><?php echo $tbi_content; ?></p>
-                        <?php endif; ?>
-
-                        <?php if ($tbi_button) : ?>
-                            <a 
-                                class="button"
-                                href="<?php echo $tbi_button['url'] ?>"
-                                target="<?php echo $tbi_button['target']; ?>"
-                            >
-                                <?php echo $tbi_button['title']; ?>
-                            </a>
-                        <?php endif; ?>
-
-                    </div>
+                    <a 
+                        href="<?php echo isset($tbi_button['url']) ? $tbi_button['url'] : '' ?>"
+                        target="<?php echo isset($tbi_button['target']) ? $tbi_button['target'] : '' ?>"
+                    >
+                        <div class="tbi-content--inner">
+                            <?php if ($tbi_title) : ?>
+                                <h3 class="tbi-title"><?php echo $tbi_title; ?></h3>
+                            <?php endif; ?>
+                            <?php if ($tbi_intro) : ?>
+                                <p class="tbi-intro"><?php echo $tbi_intro; ?></p>
+                            <?php endif; ?>
+                            <?php if ($tbi_content) : ?>
+                                <p class="tbi-body"><?php echo $tbi_content; ?></p>
+                            <?php endif; ?>
+                            <?php if ($tbi_button) : ?>
+                                <a
+                                    class="button"
+                                    href="<?php echo $tbi_button['url'] ?>"
+                                    target="<?php echo $tbi_button['target']; ?>"
+                                >
+                                    <?php echo $tbi_button['title']; ?>
+                                </a>
+                            <?php endif; ?>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
