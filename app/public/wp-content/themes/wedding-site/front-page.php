afc01874f6d666cb0c778e10733306ef70267e69
Liam McLuckie <liammcluckie@hotmail.co.uk>
2023-03-18 16:18:43 +0000

    Add cards acf data

--- app/public/wp-content/themes/wedding-site/front-page.php
+++ app/public/wp-content/themes/wedding-site/front-page.php
@@ -2,7 +2,32 @@
 
 <?php include_once 'template-parts/hero.php'; ?>
 <?php include_once 'template-parts/timeline.php'; ?>
-<?php include_once 'template-parts/cards.php'; ?>
-<?php include_once 'template-parts/text-beside-image.php'; ?>
+<?php 
+    $layout_title = 'Info Pages';
+
+    $card_one = [
+        'image' => get_field('card_image_one'),
+        'title' => get_field('card_title_one'),
+        'excerpt' => get_field('card_excerpt_one'),
+        'button' => get_field('card_link_one'),
+    ];
+
+    $card_two = [
+        'image' => get_field('card_image_two'),
+        'title' => get_field('card_title_two'),
+        'excerpt' => get_field('card_excerpt_two'),
+        'button' => get_field('card_link_two'),
+    ];
+
+    $card_three = [
+        'image' => get_field('card_image_three'),
+        'title' => get_field('card_title_three'),
+        'excerpt' => get_field('card_excerpt_three'),
+        'button' => get_field('card_link_three'),
+    ];
+
+    include get_template_directory() . '/template-parts/cards.php';
+?>
+<?php // include_once 'template-parts/text-beside-image.php'; ?>
 
 <?php get_footer(); ?>
