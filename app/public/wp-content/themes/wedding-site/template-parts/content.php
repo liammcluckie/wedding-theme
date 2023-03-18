abcd3097f62f94c592ad9e5485b994589fe88732
Liam McLuckie <liammcluckie@hotmail.co.uk>
2023-03-18 12:47:16 +0000

    Add content template part

---
+++ app/public/wp-content/themes/wedding-site/template-parts/content.php
@@ -0,0 +1,33 @@
+<section class="container no-margin-top">
+    <div class="row flex-centered">
+        <div class="column one-half xl-two-thirds m-one-whole">
+
+            <?php if ($title) : ?>
+                <h2 class="content-title"><?php echo $title; ?></h2>
+            <?php endif; ?>
+
+        </div>
+    </div>
+    <div class="row flex-centered">
+        <div class="column one-half xl-two-thirds m-one-whole no-padding-top">
+
+            <?php if ($content && $wysiwyg) : ?>
+                <?php echo $content ?>
+            <?php elseif ($content && !$wysiwyg) : ?>
+                <p class="content"><?php echo $content; ?></p>
+            <?php endif; ?>
+
+            <?php if ($button) : ?>
+                <a 
+                    class="button"
+                    href="<?php echo $button['url']; ?>"
+                    target="<?php echo $button['target']; ?>"
+                >
+                    <?php echo $button['title'] ?>
+                </a>
+
+            <?php endif; ?>
+
+        </div>
+    </div>
+</section>
